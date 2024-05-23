ed841248cb932193b93c77df1a4943c5a7b72a5c
Madhu-shree1243 <114743017+Madhu-shree1243@users.noreply.github.com>
2024-05-23 11:34:56 +0530

    done

---
+++ print.py
@@ -0,0 +1,17 @@
+# print sum of two numbers
+# a = 1000
+# b = 500
+# sum = a + b 
+# print(sum)
+
+a = 1000
+b = 500
+
+# diff = a - b
+# print(diff)
+
+# product = a*b
+# print(product)
+
+divide = a/b
+print(divide)
