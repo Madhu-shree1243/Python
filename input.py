61ae331bcf72b079031cc2a10d97137663021165
Madhu-shree1243 <114743017+Madhu-shree1243@users.noreply.github.com>
2024-05-26 13:20:34 +0530

    ok done

---
+++ input.py
@@ -0,0 +1,7 @@
+#Tking input from user & printing it ....
+
+name = input("name : ")
+age = int(input("age :"))
+price = float(input("price :"))
+
+print("My name is " , name  ,"and I am " , age,"years old ","the price of tomato is ",price)
